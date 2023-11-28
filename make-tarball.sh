645f17ba67eb0be81c19afd1f42e37dd5c3a8689
Gerd Hoffmann <kraxel@redhat.com>
2023-11-28 15:09:31 +0100

    make hashlength configurable in make-tarball.sh
    [skip changelog]

--- make-tarball.sh
+++ make-tarball.sh
@@ -3,15 +3,16 @@
 # args
 repo="${1%/}"
 ref="${2-HEAD}"
+ab="${3-12}"
 
 # check
 if test ! -d "${repo}/.git"; then
-    echo "usage: $0 <repodir> [ <ref> ]"
+    echo "usage: $0 <repodir> [ <ref> [ <abbrev> ] ]"
     exit 1
 fi
 
 # get + print info
-commit=$(cd $repo; git show --pretty='format:%h' $ref | head -1)
+commit=$(cd $repo; git show --abbrev=$ab --pretty='format:%h' $ref | head -1)
 date=$(cd $repo; git show --pretty='format:%cs' $ref | head -1 | tr -d '-')
 echo "# $repo $ref  ->  commit $commit - date $date"
 
