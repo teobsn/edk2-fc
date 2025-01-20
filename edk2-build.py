a536b3fa1fbaa59a62d1ddd3509e36b4a6f49572
Gerd Hoffmann <kraxel@redhat.com>
2025-01-20 16:13:36 +0100

    minor build script update

--- edk2-build.py
+++ edk2-build.py
@@ -51,7 +51,7 @@ def get_toolchain(cfg, build):
         return cfg[build]['tool']
     if cfg.has_option('global', 'tool'):
         return cfg['global']['tool']
-    return 'GCC5'
+    return 'GCC'
 
 def get_hostarch():
     mach = os.uname().machine
