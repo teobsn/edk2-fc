83fc627799e32b10d7043da675079ca2173baed3
Gerd Hoffmann <kraxel@redhat.com>
2025-06-13 13:29:16 +0200

    add make-dbxupdate.sh script

---
+++ make-dbxupdate.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# https://github.com/microsoft/secureboot_objects.git
+sbrepo="$HOME/projects/secureboot"
+
+dbxdir="PostSignedObjects/DBX"
+srcdir="${sbrepo}/${dbxdir}"
+
+test -d "$srcdir" || exit 1
+
+echo "#"
+echo "# pull repo updates"
+(set -x; cd "$sbrepo"; git pull)
+commit=$(cd "$sbrepo"; git log --oneline -n 1 --pretty='format:%h' -- $dbxdir)
+latest=$(cd "$sbrepo"; git log --oneline -n 1 --pretty='format:%cs' -- $dbxdir | tr -d '-')
+current=$(awk '/%define DBXDATE/ { print $3 }' edk2.spec)
+if test "$current" = "$latest"; then
+    echo "#"
+    echo "# no update needed ($latest)"
+    echo "#"
+    exit 0
+fi
+
+echo "#"
+echo "# need dbx update"
+echo "#   current : $current"
+echo "#   latest  : $latest"
+
+echo "#"
+echo "# copy updates"
+cp -v "${srcdir}/x86/DBXUpdate.bin" "DBXUpdate-${latest}.ia32.bin"
+cp -v "${srcdir}/amd64/DBXUpdate.bin" "DBXUpdate-${latest}.x64.bin"
+cp -v "${srcdir}/arm64/DBXUpdate.bin" "DBXUpdate-${latest}.aa64.bin"
+
+echo "#"
+echo "# update specfile and git"
+sed -e "/%define DBXDATE/s/20.*/$latest/" -i edk2.spec
+git add DBXUpdate-${latest}.*.bin
+git add edk2.spec
+git commit -m "update dbx to $latest"
+
+echo "#"
+echo "# show update commit"
+git show
