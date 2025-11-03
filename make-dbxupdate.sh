cecad78d115de5fa1ac46ec06009889b96997e86
Gerd Hoffmann <kraxel@redhat.com>
2025-11-03 09:36:08 +0100

    dbxupdate: add version tag info

--- make-dbxupdate.sh
+++ make-dbxupdate.sh
@@ -13,10 +13,11 @@ echo "# pull repo updates"
 (set -x; cd "$sbrepo"; git pull)
 commit=$(cd "$sbrepo"; git log --oneline -n 1 --pretty='format:%h' -- $dbxdir)
 latest=$(cd "$sbrepo"; git log --oneline -n 1 --pretty='format:%cs' -- $dbxdir | tr -d '-')
+version=$(cd "$sbrepo"; git describe --tags --long --match v* | cut -d- -f1)
 current=$(awk '/%define DBXDATE/ { print $3 }' edk2.spec)
 if test "$current" = "$latest"; then
     echo "#"
-    echo "# no update needed ($latest)"
+    echo "# no update needed ($latest, $version)"
     echo "#"
     exit 0
 fi
@@ -24,7 +25,7 @@ fi
 echo "#"
 echo "# need dbx update"
 echo "#   current : $current"
-echo "#   latest  : $latest"
+echo "#   latest  : $latest ($version)"
 
 echo "#"
 echo "# copy updates"
@@ -36,7 +37,7 @@ echo "# update specfile and git"
 sed -e "/%define DBXDATE/s/20.*/$latest/" -i edk2.spec
 git add DBXUpdate-${latest}.*.bin
 git add edk2.spec
-git commit -m "update dbx to $latest"
+git commit -m "update dbx to $latest ($version)"
 
 echo "#"
 echo "# show update commit"
